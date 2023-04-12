03bff82ff62cab6d84ec2abbd6f0134d25ef6317
vkasyan76 <vkasyan@gmx.de>
2023-04-12 20:31:04 +0200

    Dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./scenes/navbar";
+import Dashboard from "./scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -16,7 +17,7 @@ function App() {
           <Box width="100" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route
                 path="/predictions"
                 element={<div>Predictions page</div>}
