11435d399788d1a00c9305b7d05a3661c51575d9
vkasyan76 <vkasyan@gmx.de>
2023-04-30 22:31:19 +0200

    Data Grid - Transactions

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -30,18 +30,48 @@ const productColumns = [
   },
 ];
 
+const transactionColumns = [
+  {
+    field: "_id",
+    headerName: "id",
+    flex: 1,
+  },
+  {
+    field: "buyer",
+    headerName: "Buyer",
+    flex: 0.67,
+  },
+  {
+    field: "amount",
+    headerName: "Amount",
+    flex: 0.35,
+    renderCell: (params: GridCellParams) => `$${params.value}`,
+  },
+  {
+    field: "productIds",
+    headerName: "Count",
+    flex: 0.1,
+    // renderCell: (params: GridCellParams) => params.value.length,
+    renderCell: (params: GridCellParams) =>
+      (params.value as Array<string>).length,
+  },
+];
+
 const Row3 = () => {
   const { palette } = useTheme();
   const { data: operationalData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
-  const { data: transactionsData } = useGetTransactionsQuery();
-  console.log("data:", transactionsData);
+  const { data: transactionData } = useGetTransactionsQuery();
+  console.log("data:", transactionData);
 
   return (
     <>
       {/* TABLE 1 */}
       <DashboardBox bgcolor="#fff" gridArea="g">
-        <BoxHeader title="List of Products" sideText="+4%" />
+        <BoxHeader
+          title="List of Products"
+          sideText={`${productData?.length} products`}
+        />
         <Box
           mt="0.5rem"
           p="0 0.5rem"
@@ -74,7 +104,45 @@ const Row3 = () => {
           />
         </Box>
       </DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="h"></DashboardBox>
+
+      {/* TABLE 2 */}
+      <DashboardBox bgcolor="#fff" gridArea="h">
+        <BoxHeader
+          title="List of Products"
+          sideText={`${transactionData?.length} latest transactions`}
+        />
+        <Box
+          mt="1rem"
+          p="0 0.5rem"
+          height="80%"
+          sx={{
+            "& .MuiDataGrid-root": {
+              color: palette.grey[300],
+              border: "none",
+            },
+            // "& .MuiDataGrid-toolbarContainer:hover .MuiButton-text": {
+            //   color: `${palette.secondary[800]} !important`,
+            // },
+            "& .MuiDataGrid-cell": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            "& .MuiDataGrid-columnSeparator": {
+              visibility: "hidden",
+            },
+          }}
+        >
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={transactionData || []}
+            columns={transactionColumns}
+          />
+        </Box>
+      </DashboardBox>
       <DashboardBox bgcolor="#fff" gridArea="i"></DashboardBox>
       <DashboardBox bgcolor="#fff" gridArea="j"></DashboardBox>
     </>
